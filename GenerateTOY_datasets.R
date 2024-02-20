f416dad43f4c4d984be51629f7e4ddd05c9d09b8
WillhKessler <willhkessler+github@gmail.com>
2024-02-20 13:37:20 -0500

    update comment wording

--- GenerateTOY_datasets.R
+++ GenerateTOY_datasets.R
@@ -11,21 +11,39 @@ data(us_states)                               ##
 ##--------------------------------------------##
 
 ## INPUTS
-npnts<-100
-reps<- 1
-startdate<- "2005-01-01"
+npnts<-100 #number of observations in sample
+npartic<-34
+reps<- 1 # how many replicates of this dataset
+firststartdate<- "2012-01-01" # Earliest starting observation
 # startdate<- "1984-01-01"
 # enddate<- "2022-12-31"
-enddate<- "2005-12-31"
-#period<- "random" #Number of consecutive days in observation period. alternatively, enter a number
-period<- 10 #Number of consecutive days in observation period. alternatively, enter a number
+laststartdate<- "2022-12-31" # Last starting observation date, if this is equal to 'firststartdate'
+period<- "random" #Number of consecutive days in observation period. Use the string 'random' for uniform random number up to 2 years, alternatively, enter an integer
 
 outputdir<- "S:/GCMC/_Code/TESTING_datasets/"
-outputname<-"toyCohort_nurses"
-format<-"csv" 
+outputname<-"VITAL_toycohort"
+format<-"csv"
+IDfield<- 'subject_ID'
+xfield<-'x'
+yfield<-'y'
+startdatefield<-"start_date"
+enddatefield<-"stop_date"
 
+
+###################################################################################################################
 ## ToY FUNCTIONS
-generateCohort<- function(npnts= npnts, startdate=startdate,enddate=enddate,outputdir,outputname, format = "RDS"){
+generateCohort<- function(npnts= npnts, 
+                          npartic = npartic,
+                          startdate=firststartdate,
+                          enddate=firstenddate,
+                          outputdir,
+                          outputname, 
+                          IDfield="UUID",
+                          yfield='latitude',
+                          xfield='longitude',
+                          startdatefield='start_date',
+                          enddatefield='end_date',
+                          format = "RDS"){
 
 ## Generate Points within the specified Geometry
 geo <- sf::as_Spatial(us_states[us_states$NAME %in% c("Alaska","Hawaii")==FALSE,][1])
@@ -34,18 +52,34 @@ points<- spsample(geo, n=npnts, "random")
 
 # Generate random dates
 num_dates <- npnts  # Number of dates to generate
+
 if (period =="random"){
-rand_startdates <- sample(seq(as.Date(startdate),as.Date(enddate),by="day"), num_dates, replace = TRUE)
-rand_enddates<-rand_startdates+round(rnorm(npnts,mean=runif(1,1,365*2),sd=10))
+  
+  rand_startdates <- sample(seq(as.Date(startdate),as.Date(enddate),by="day"), num_dates, replace = TRUE)
+  rand_enddates<-rand_startdates+round(rnorm(npnts,mean=runif(1,1,365*2),sd=10))
+
 }else if (is.numeric(period)){
+  
   rand_startdates <- sample(seq(as.Date(startdate),as.Date(enddate),by="day"), num_dates, replace = TRUE)
   rand_enddates<-rand_startdates+period
   #rand_enddates<-sapply(rand_startdates, FUN = function(x){as.Date(x)+period})
-}else{rand_startdates <- sample(seq(as.Date(startdate),as.Date(enddate),by="day"), num_dates, replace = TRUE)
-rand_enddates<-rand_startdates}
+  
+}else{
+  rand_startdates <- sample(seq(as.Date(startdate),as.Date(enddate),by="day"), num_dates, replace = TRUE)
+  rand_enddates<-rand_startdates
+}
 
+# Create UUIDs
+if (npnts==npartic){
+  UUIDs <-1:npnts
+}else{
+  UUIDs<-sample(1:npartic,npnts,replace=TRUE)
+}
 # Create "cohort" by adding date columns to point data
-cohort<-data.frame(UUID= 1:npnts , latitude = points@coords[,"y"], longitude = points@coords[,"x"], start_date = rand_startdates, end_date = rand_enddates)
+cohort<-data.frame(UUID= UUIDs , latitude = points@coords[,"y"], longitude = points@coords[,"x"], start_date = rand_startdates, end_date = rand_enddates)
+
+# assign fieldnames
+colnames(cohort)<-c(IDfield, yfield, xfield,startdatefield,enddatefield)
 
 
 
@@ -61,18 +95,17 @@ if (format == "RDS"){
 }
 
 
-# generateCohort(
-#   npnts = npnts,
-#   startdate = startdate,
-#   enddate = enddate,
-#   outputdir = outputdir, 
-#   outputname = outputname, 
-#   format = "csv")
 
 sapply(1:reps,function(x){generateCohort(
-  npnts = npnts,
-  startdate = startdate,
-  enddate = enddate,
-  outputdir = outputdir, 
-  outputname = outputname, 
+  npnts= npnts, 
+  npartic = npartic,
+  startdate=firststartdate,
+  enddate=laststartdate,
+  outputdir=outputdir,
+  outputname=outputname, 
+  IDfield=IDfield,
+  yfield=yfield,
+  xfield=xfield,
+  startdatefield=startdatefield,
+  enddatefield=enddatefield, 
   format = "csv")})
