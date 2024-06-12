9f09b8b6238a4ecd430466476499b54de3af3564
WillhKessler <37121681+WillhKessler@users.noreply.github.com>
2024-06-12 16:36:16 -0400

    Update ParallelSocket_processingtemplate.R

--- ParallelSocket_processingtemplate.R
+++ ParallelSocket_processingtemplate.R
@@ -25,7 +25,7 @@ for (i in listOfPackages){
      if(! i %in% installed.packages()){
          install.packages(i, dependencies = TRUE)
      }
-     require(i)
+     require(i,character.only=TRUE)
 }
 
 
