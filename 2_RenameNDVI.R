051407f07ca4d959acf1c3847214a029cdf122fb
WillhKessler <willhkessler+github@gmail.com>
2024-08-06 12:14:16 -0400

    update paths

--- 2_RenameNDVI.R
+++ 2_RenameNDVI.R
@@ -5,13 +5,14 @@ require('tools')
 
 #List all NDVI files
 files<- list.files("E:\\.shortcut-targets-by-id\\1h9cmIyzx_TtG1VTHvrQ_Cq2IQrJzIwqF\\NDVI",pattern = "*.tif$",full.names = TRUE,recursive = TRUE,include.dirs = FALSE)
+#files<- list.files("H:\\My Drive\\NDVI",pattern = "*.tif$",full.names = TRUE,recursive = TRUE,include.dirs = FALSE)
 outdir<-"S:\\GCMC\\Data\\Greenness\\NDVI\\"
 
 n=1
 #Loop through files
 nfiles<- length(files)
 for(i in files){
-  print(paste("processing:", n, "of ",nfiles,sep=""))
+  print(paste("processing: ", n, " of ",nfiles,sep=""))
   #Check that file name roughly follows correct format
   if(length(strsplit(basename(i),"_")[[1]])>2){
     
@@ -20,8 +21,7 @@ for(i in files){
     print(oldname)
     #Pull the resolution from the old file name
     res=oldname[2]
-    print("resolution: ")
-    print(res)
+    print(paste("resolution: ",res))
     
     if(oldname[2]=="30"){
       #Get the NDVI regions from the Data archive
@@ -29,12 +29,9 @@ for(i in files){
       #Use fuzzy matching to match file name to a region
       region=matchregions[amatch(basename(oldname[1]),matchregions,nomatch=1,maxDist = 6)]
       newname=paste(oldname[-2],collapse='_')
-      print("oldfile: ")
-      print(i)
-      print("new file name: ")
-      print(newname)
-      print("new file path:" )
-      print(paste0(outdir,res,"m","\\",region,"\\",newname))
+      print(paste("oldfile: ",i))
+      print(paste("new file name: ",newname))
+      print(paste("new file path:",paste0(outdir,res,"m","\\",region,"\\",newname)))
       file.copy(i,to=paste0(outdir,res,"m","\\",region,"\\",newname))
       
     }else{
@@ -44,12 +41,9 @@ for(i in files){
       #Use fuzzy matching to match file name to a region
       region=matchregions[amatch(basename(oldname[1]),matchregions,nomatch=1,maxDist = 6)]
       newname=paste(oldname,collapse="_") 
-      print("oldfile: ")
-      print(i)
-      print("new file name: ")
-      print(newname)
-      print("new file path:" )
-      print(paste0(outdir,"focalstats_",res,"m","\\",region,"\\",newname))
+      print(paste("oldfile: ",i))
+      print(paste("new file name: ",newname))
+      print(paste("new file path: ",paste0(outdir,"focalstats_",res,"m","\\",region,"\\",newname)))
       file.copy(i,to=paste0(outdir,"focalstats_",res,"m","\\",region,"\\",newname))
       
     }
@@ -68,3 +62,4 @@ for(i in files){
   print(i)}
 n=n+1     
 }
+
