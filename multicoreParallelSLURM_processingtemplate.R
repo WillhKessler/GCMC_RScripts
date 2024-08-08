8a072c6485246d8497aa7350ce5434be67890551
WillhKessler <willhkessler+github@gmail.com>
2024-08-08 13:43:51 -0400

    Update Processing Template, group features into lists of jobs for multicore internal parallelization

--- multicoreParallelSLURM_processingtemplate.R
+++ multicoreParallelSLURM_processingtemplate.R
@@ -1,17 +1,16 @@
 ##---- REQUIRED INPUTS ----##
 PROJECT_NAME<-"innerParallelTest" # string with a project name
-rasterdir<- "/pc/nhair0a/Built_Environment/BE_Data/Geographic_Data/PRISM_daily/PRISM_data/an" # string with a file path to raster covariates to extract- function will try to pull variable names from sub directories i.e /PRISM/ppt or /PRISM/tmean or /NDVI/30m
-extractionlayer = "/d/tmp/nhairs/nhair0a/BellaviaLinkage/sites_10M/sites_10M.shp" # string with path to spatial layer to use for extraction. Can be a CSV or SHP or GDB 
+rasterdir<- "S:/GCMC/Data/Climate/PRISM/daily" # string with a file path to raster covariates to extract- function will try to pull variable names from sub directories i.e /PRISM/ppt or /PRISM/tmean or /NDVI/30m
+extractionlayer = "S:\\GCMC\\_Code\\TESTING_datasets\\csv\\toyCohort_nurses51.csv" # string with path to spatial layer to use for extraction. Can be a CSV or SHP or GDB 
 layername = "sites_10M" # Layer name used when extraction layer is an SHP or GDB
-IDfield<-"ORIG_FID" # Field in extraction layer specifying IDs for features, can be unique or not, used to chunk up batch jobs
-Xfield<- "X"
-Yfield<- "Y"
+IDfield<-"UUID" # Field in extraction layer specifying IDs for features, can be unique or not, used to chunk up batch jobs
+Xfield<- "longitude"
+Yfield<- "latitude"
 startdatefield = "start_date" # Field in extraction layer specifying first date of observations
 enddatefield = "end_date" # Field in extraction layer specifying last date of observations
 predays = 0 # Integer specifying how many days preceding 'startingdatefield' to extract data. i.e. 365 will mean data extraction will begin 1 year before startdatefield
 weights = NA # string specifying file path to raster weights, should only be used when extraction layer is a polygon layer
-
-
+partition = "linux01"
 
 ##---- Required Packages
 library(batchtools)
@@ -46,25 +45,30 @@ source("https://raw.githubusercontent.com/WillhKessler/GCMC_RScripts/main/Functi
 ##############################################################
 ##---- Set up the batch processing jobs
 ##---- Use the 'batchgrid' function to create a grid of variable combinations to process over. function considers input rasters, input features, and any weighting layers
-
-batchgrid = function(rasterdir,extractionlayer,layername,IDfield,Xfield,Yfield,startdatefield,enddatefield,predays,weightslayers){
+jobgrid = function(rasterdir,extractionlayer,layername,IDfield,Xfield,Yfield,startdatefield,enddatefield,predays,weightslayers,pbatch){
   require("tools")
   
   ##---- Set up the batch processing jobs
   pvars = list.dirs(path = rasterdir,full.names = FALSE,recursive = FALSE)
   
   if(file_ext(extractionlayer)=="csv"){
-    feature<-unique(read.csv(extractionlayer)[,IDfield])
+    vectorfile<-read.csv(extractionlayer)
+    feature<-vectorfile[,IDfield]
+    #feature<-unique(vectorfile[,IDfield])
     layername = NA
     weightslayers = NA
+    cpu = c(1:4)
+    if(partition=="linux01"){
+      pbatch = c(1:10)
+    }
+    
   }else if(file_ext(extractionlayer) %in% c("gdb")){
     require('terra')
     vectorfile<- vect(x=extractionlayer,layer=layername)
     feature<- unlist(unique(values(vectorfile[,IDfield])))
     Xfield = NA
     Yfield = NA
-  }
-  else if(file_ext(extractionlayer) %in% c("shp")){
+  }else if(file_ext(extractionlayer) %in% c("shp")){
     require('terra')
     vectorfile<- vect(x=extractionlayer)
     feature<- unlist(unique(values(vectorfile[,IDfield])))
@@ -73,20 +77,66 @@ batchgrid = function(rasterdir,extractionlayer,layername,IDfield,Xfield,Yfield,s
   }
   
   output<- expand.grid(vars = pvars,
-                     piece = feature,
-                     rasterdir = rasterdir,
-                     extractionlayer = extractionlayer,
-                     layername = layername,
-                     IDfield = IDfield,
-                     Xfield = Xfield,
-                     Yfield = Yfield,
-                     startdatefield = startdatefield,
-                     enddatefield = enddatefield,
-                     predays = predays,
-                     weightslayers = weightslayers,
-                     stringsAsFactors = FALSE)
+                       pieces = split(
+                         x = split(x = feature, f = ceiling(seq_along(feature)/100)),
+                         f = ceiling(seq_along(split(feature, ceiling(seq_along(feature)/100)))/sum(length(pbatch),length(cpu)))
+                         ),
+                       #pieces = split(feature, ceiling(seq_along(feature)/sum(length(pbatch),length(cpu)))),
+                       #piece = feature,
+                       rasterdir = rasterdir,
+                       extractionlayer = extractionlayer,
+                       layername = layername,
+                       IDfield = IDfield,
+                       Xfield = Xfield,
+                       Yfield = Yfield,
+                       startdatefield = startdatefield,
+                       enddatefield = enddatefield,
+                       predays = predays,
+                       weightslayers = weightslayers,
+                       stringsAsFactors = FALSE)
   return(output)
-}
+  }
+
+# batchgrid = function(rasterdir,extractionlayer,layername,IDfield,Xfield,Yfield,startdatefield,enddatefield,predays,weightslayers){
+#   require("tools")
+#   
+#   ##---- Set up the batch processing jobs
+#   pvars = list.dirs(path = rasterdir,full.names = FALSE,recursive = FALSE)
+#   
+#   if(file_ext(extractionlayer)=="csv"){
+#     feature<-unique(read.csv(extractionlayer)[,IDfield])
+#     layername = NA
+#     weightslayers = NA
+#   }else if(file_ext(extractionlayer) %in% c("gdb")){
+#     require('terra')
+#     vectorfile<- vect(x=extractionlayer,layer=layername)
+#     feature<- unlist(unique(values(vectorfile[,IDfield])))
+#     Xfield = NA
+#     Yfield = NA
+#   }
+#   else if(file_ext(extractionlayer) %in% c("shp")){
+#     require('terra')
+#     vectorfile<- vect(x=extractionlayer)
+#     feature<- unlist(unique(values(vectorfile[,IDfield])))
+#     Xfield = NA
+#     Yfield = NA
+#   }
+#   
+#   output<- expand.grid(vars = pvars,
+#                      piece = feature,
+#                      rasterdir = rasterdir,
+#                      extractionlayer = extractionlayer,
+#                      layername = layername,
+#                      IDfield = IDfield,
+#                      Xfield = Xfield,
+#                      Yfield = Yfield,
+#                      startdatefield = startdatefield,
+#                      enddatefield = enddatefield,
+#                      predays = predays,
+#                      weightslayers = weightslayers,
+#                      stringsAsFactors = FALSE)
+#   return(output)
+# }
 
 ##----  Make sure registry is empty
 clearRegistry(reg)
