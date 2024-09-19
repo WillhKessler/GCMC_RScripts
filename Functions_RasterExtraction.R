416b0e7f11e137afc0eb8bc5491fa81aca0dea61
WillhKessler <willhkessler+github@gmail.com>
2024-09-19 12:55:26 -0400

    Add function combine.results() to Functions_RasterExtraction.R for recombining outputs from parallel processing

--- Functions_RasterExtraction.R
+++ Functions_RasterExtraction.R
@@ -414,7 +414,39 @@ simple.extract.rast= function(vars,piece,rasterdir,extractionlayer,layername,IDf
 
 
 ##---- Function to append all Recombine Outputs from Parallelization
-combine.results= function(x){
+combine.results= function(projectname=PROJECT_NAME,reg=reg){
+  require('batchtools')
+  require('tidyr')
+  
+  ##---- Load Registry
+  reg<- loadRegistry(reg)
+  
+  ##---- Create Jobs Table
+  jobs<-getJobPars(reg=reg)
+  jobpars<-as.data.frame(lapply(data.frame(do.call(rbind,jobs$job.pars)),unlist))
+  jobpars2<-jobpars
+  
+  jobpars2[]<-lapply(jobpars2[],as.character)
+  
+  
+  ##---- Combine all the outputs into a dataframe
+  
+  results<- do.call("rbind",lapply(1:nrow(jobs),loadResult))
+  #results<- do.call("rbind",lapply(1:20,loadResult))
+  
+  for(v in as.character(unique(unlist(results[,1])))){
+    out<- lapply(results[as.character(unlist(results[,1]))==v,3],terra::unwrap)
+    out2<- lapply(out,terra::as.data.frame)
+    out3<-Reduce(function(dtf1,dtf2){merge(dtf1,dtf2,all=TRUE)},out2)
+    longout<-lapply(out2,function(x){reshape2::melt(melt(x,id.vars=colnames(x)[!grepl("^\\d{4}\\-\\d{2}\\-\\d{2}\\b",colnames(x))],
+                                                         variable.names="date",value.name=v,na.rm=FALSE))})
+    longout<- lapply(out2,function(x){as.data.frame(x%>% pivot_longer(cols= colnames(x)[grepl("^\\d{4}\\-\\d{2}\\-\\d{2}\\b",colnames(x))],names_to = "date",values_to = v))})
+   
+    longout<-do.call("rbind",longout)
+    write.csv(longout,paste("_LONG",".csv",sep=""))
+    write.csv(out3,paste(projectname,"_",v,".csv",sep=""))
+    saveRDS(out3,file=paste(projectname,"_",v,".rds",sep=""))
+  }
   
 }
 
