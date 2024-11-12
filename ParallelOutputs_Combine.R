1e4f064283a7b69143c9b344881f2a2496112657
WillhKessler <37121681+WillhKessler@users.noreply.github.com>
2024-11-12 13:18:56 -0500

    Update ParallelOutputs_Combine.R

--- ParallelOutputs_Combine.R
+++ ParallelOutputs_Combine.R
@@ -6,25 +6,26 @@ reg<- loadRegistry('VITAL_NDVI_Registry')
 
 ##---- Create Jobs Table
 jobs<-getJobPars(reg=reg)
-#jobpars<-as.data.frame(lapply(data.frame(do.call(rbind,jobs$job.pars)),unlist))
-#jobpars2<-jobpars
-
-#jobpars2[]<-lapply(jobpars2[],as.character)
-
 
 ##---- Combine all the outputs into a dataframe
-
 results<- do.call("rbind",lapply(1:nrow(jobs),loadResult))
 
+# Loop through all unique variables , writing results to file
 for(v in as.character(unique(unlist(results[,1])))){
+  # Load and unwrap the spatial vector results
   out<- lapply(results[as.character(unlist(results[,1]))==v,3],terra::unwrap)
+
+  # Convert results to list of dataframes 
   out2<- lapply(out,terra::as.data.frame)
+
+  # Reduce all outputs to a dataframe
   out3<-Reduce(function(dtf1,dtf2){merge(dtf1,dtf2,all=TRUE)},out2)
-  longout<-lapply(out2,function(x){reshape2::melt(melt(x,id.vars=colnames(x)[!grepl("^\\d{4}\\-\\d{2}\\-\\d{2}\\b",colnames(x))],
-                                                       variable.names="date",value.name=v,na.rm=FALSE))})
+
+  # Convert to LONG format
   longout<- lapply(out2,function(x){as.data.frame(x%>% pivot_longer(cols= colnames(x)[grepl("^\\d{4}\\-\\d{2}\\-\\d{2}\\b",colnames(x))],names_to = "date",values_to = v))})
   longout<-do.call("rbind",longout)
-  
+
+  # Write tabular outputs: wide, long, RDS
   write.csv(longout,paste("VITAL_NDVIDAILY_LONG",v,".csv",sep=""))
   write.csv(out3,paste("VITAL_NDVIDAILY_",v,".csv",sep=""))
   saveRDS(out3,file=paste("VITAL_NDVIDAILY_",v,".rds",sep=""))
