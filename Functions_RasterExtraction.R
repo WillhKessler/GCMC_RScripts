9935be36dfec8b0a7019900d9b7c6088ed270e8a
WillhKessler <37121681+WillhKessler@users.noreply.github.com>
2024-12-04 10:36:54 -0500

    Update Functions_RasterExtraction.R

--- Functions_RasterExtraction.R
+++ Functions_RasterExtraction.R
@@ -1,8 +1,8 @@
 ##---- Check SLURM partitions
-get.partitions <- function(partition){
+#get.partitions <- function(partition){
   # Check if supplied partition name is in the list of available partitions
-  if(partition %in% trimws(system("sinfo -O PartitionName",intern=TRUE)[1])){return TRUE}else{return FALSE}
-}
+#  if(partition %in% trimws(system("sinfo -O PartitionName",intern=TRUE)[1])){return TRUE}else{return FALSE}
+#}
 
 
 ##---- Load Required Packages
