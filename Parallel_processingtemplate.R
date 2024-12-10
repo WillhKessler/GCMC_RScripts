b9ff3a382a8138ced6e4c8d01db94f3848f82deb
WillhKessler <willhkessler+github@gmail.com>
2024-12-10 12:13:39 -0500

    update projectdirectory assignment, update period assignment to daily update source()

--- Parallel_processingtemplate.R
+++ Parallel_processingtemplate.R
@@ -4,7 +4,7 @@
 # The required inputs are standardized for ALL functions defined in the Raster Extractions R source file 
 
 ##---- REQUIRED INPUTS ----##
-projectdirectory = "" # This is where all your outputs, registry items, and intermediate files will be created
+projectdirectory = getwd() # This is where all your outputs, registry items, and intermediate files will be created
 scheduler = "socket" # String to set where the jobs will run. i.e. SLURM scheduler on cluster, multicore workstation, or local interactive session. Can be values of "SLURM", "interactive","socket"
 PROJECT_NAME = "simple_ParallelTest" # String with a project name
 rasterdir = "S:/GCMC/Data/Climate/PRISM/daily" # string with a file path to raster covariates to extract- function will try to pull variable names from sub directories i.e /PRISM/ppt or /PRISM/tmean or /NDVI/30m
@@ -17,10 +17,11 @@ startdatefield = "start_date" # Field in extraction layer specifying first date 
 enddatefield = "end_date" # Field in extraction layer specifying last date of observations
 predays = 0 # Integer specifying how many days preceding 'startingdatefield' to extract data. i.e. 365 will mean data extraction will begin 1 year before startdatefield
 weights = NA # string specifying file path to raster weights, should only be used when extraction layer is a polygon layer
+period = "daily" #specify a period to summarize values: daily, monthly, yearly, defaults to daily
 email = "" # enter your email and SLURM will send you an email when each chunk of jobs is complete
 
 ## Load Required Functions
-source("https://raw.githubusercontent.com/WillhKessler/GCMC_RScripts/main/Functions_RasterExtraction.R")
+source("https://raw.githubusercontent.com/WillhKessler/GCMC_RScripts/refs/heads/main/Functions_RasterExtraction.R")
 
 ## select desired cluster function  
 reg=select.Cluster(scheduler = scheduler)
