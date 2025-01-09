39b935dbacd94d20e89547cea488cce0e5c576af
WillhKessler <willhkessler+github@gmail.com>
2025-01-09 10:20:06 -0500

    rasterize polygons before performing zonal statistics

--- Functions_RasterExtraction.R
+++ Functions_RasterExtraction.R
@@ -331,11 +331,13 @@ extract.rast= function(vars,period,piece,rasterdir,extractionlayer,layername,IDf
   if(is.polygons(polygons)){
     if(is.na(weightslayers)){
       polygons<-project(polygons,crs(rasters))
-      rasters2<- crop(x = rasters, y = polygons,snap = 'out')
-      tempoutput<-zonal(x=rasters2,z=polygons,fun=mean,na.rm=TRUE,as.polygons=TRUE)
-      tempnames<-names(tempoutput)
-      
-      output<-cbind(polygons,tempoutput)
+      rasterizedpolygons<-rasterize(polygons, rasters, field = IDfield)
+      #rasters2<- crop(x = rasters, y = polygons,snap = 'out')
+      #tempoutput<-zonal(x=rasters2,z=polygons,fun=mean,na.rm=TRUE,as.polygons=TRUE)
+      tempoutput<-zonal(x=rasters2,z=rasterizedpolygons,fun=mean,na.rm=TRUE)
+      #tempnames<-names(tempoutput)
+      #output<-cbind(polygons,tempoutput)
+      output<-merge(polygons,tempoutput,all.x=T,by.x=IDfield,by.y=IDfield)
       longoutput<-reshape2::melt(as.data.frame(output),id.vars=names(polygons),variable.names="date",value.name=vars,na.rm=FALSE)
       
     }else{
