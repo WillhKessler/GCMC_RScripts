d9a84213704deeb45c67bce50cfb9ddc69d3f12b
WillhKessler <willhkessler+github@gmail.com>
2025-06-09 13:27:16 -0400

    path change

--- Unzip_Directories.R
+++ Unzip_Directories.R
@@ -4,7 +4,7 @@ require(utils)
 #########################################
 ##---Required Inputs---------############
 #zipdir<-"S:\\GCMC\\Data\\AirPollution\\ec"
-zipdir<-"S:\\GCMC\\Data\\Climate\\PRISM\\4km\\tmax"
+zipdir<-"S:\\GCMC\\Data\\Climate/PRISM/4km/"
 
 #########################################
 ##---- Clipping Boundary
@@ -13,7 +13,9 @@ zipdir<-"S:\\GCMC\\Data\\Climate\\PRISM\\4km\\tmax"
 allzip<- list.files(path = zipdir,pattern = "*.zip$",all.files = TRUE,full.names = TRUE,recursive = TRUE,include.dirs = FALSE)
 allzipdir<-file_path_sans_ext(allzip)
 
-allunzip<-list.dirs(path=zipdir,full.names=TRUE)
+#allunzip<-list.dirs(path=zipdir,full.names=TRUE)
+allunzip<-dirname(allzipdir)
+
 remaining<- allzip[!(allzipdir %in% allunzip)]
 head(allzip)
 
@@ -22,7 +24,7 @@ head(allzip)
 for(i in remaining){
   #dirout <- file_path_sans_ext(basename(i))
   unzip(zipfile = i, exdir = dirname(i),overwrite = TRUE,)
-  
+  print(paste("unzipping:",i))
   file.remove(i)
 }
 print("Done!")
