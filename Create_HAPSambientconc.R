3eb597a42b39764cd530106a9be7c4cea76d2855
WillhKessler <willhkessler+github@gmail.com>
2025-08-11 15:22:53 -0400

    Update

--- Create_HAPSambientconc.R
+++ Create_HAPSambientconc.R
@@ -8,13 +8,13 @@
 # 
 # 
 # ####################################################
-dat<-read.csv("S:\\GCMC\\Data\\AirPollution\\HAPs\\AmbientConcentrations\\HAPs_AmbientConcentrations\\2011\\2011_Toxics_long.csv",as.is = T,stringsAsFactors = F)
-strpdat<-dat[,c("Tract","Pollutant.Name","Total.Conc")]
-widedat<-reshape(strpdat,idvar="Tract",timevar="Pollutant.Name",direction='wide')
-names(widedat)[grep(pattern="Total.Conc.",names(widedat))]<-gsub(pattern="Total.Conc.","",names(widedat)[grep(pattern="Total.Conc.",names(widedat))])
-
-widedat2<-merge(x=widedat,y=unique(dat[,c("State","EPA.Region","County","FIPS","Population","Tract")]),by="Tract",all.x=T)
-write.csv(widedat2,"S:\\GCMC\\Data\\AirPollution\\HAPs\\AmbientConcentrations\\HAPs_AmbientConcentrations\\2011\\2011_Toxics_Ambient_Concentrations.csv")
+# dat<-read.csv("S:\\GCMC\\Data\\AirPollution\\HAPs\\AmbientConcentrations\\HAPs_AmbientConcentrations\\2011\\2011_Toxics_long.csv",as.is = T,stringsAsFactors = F)
+# strpdat<-dat[,c("Tract","Pollutant.Name","Total.Conc")]
+# widedat<-reshape(strpdat,idvar="Tract",timevar="Pollutant.Name",direction='wide')
+# names(widedat)[grep(pattern="Total.Conc.",names(widedat))]<-gsub(pattern="Total.Conc.","",names(widedat)[grep(pattern="Total.Conc.",names(widedat))])
+# 
+# widedat2<-merge(x=widedat,y=unique(dat[,c("State","EPA.Region","County","FIPS","Population","Tract")]),by="Tract",all.x=T)
+# write.csv(widedat2,"S:\\GCMC\\Data\\AirPollution\\HAPs\\AmbientConcentrations\\HAPs_AmbientConcentrations\\2011\\2011_Toxics_Ambient_Concentrations.csv")
 # 
 # 
 # ####################################################
@@ -38,14 +38,16 @@ write.csv(widedat2,"S:\\GCMC\\Data\\AirPollution\\HAPs\\AmbientConcentrations\\H
 # write.csv(widedat2,"S:\\GCMC\\Data\\AirPollution\\HAPs\\AmbientConcentrations\\2002\\2002_Toxics_Ambient_Concentrations.csv")
 
 # ####################################################
-# dat<-read.csv("S:\\GCMC\\Data\\AirPollution\\HAPs\\AmbientConcentrations\\1999\\1999_long.csv",stringsAsFactors = F)
-# dat$GEOID<-paste0(dat$FIPS,dat$TRACT)
-# strpdat<-dat[,c("tract_id","hapname","ASPENTotal")]
-# widedat<-reshape(strpdat,idvar="tract_id",timevar="hapname",direction='wide')
-# names(widedat)[grep(pattern="ASPENTotal.",names(widedat))]<-gsub(pattern="ASPENTotal.","",names(widedat)[grep(pattern="ASPENTotal.",names(widedat))])
-# 
-# widedat2<-merge(x=widedat,y=unique(dat[,c("tract_id","county","uflag")]),by="tract_id",all.x=T)
-# write.csv(widedat2,"S:\\GCMC\\Data\\AirPollution\\HAPs\\AmbientConcentrations\\1999\\1999_Toxics_Ambient_Concentrations.csv")
+dat<-read.csv("S:\\GCMC\\Data\\AirPollution\\HAPs\\AmbientConcentrations\\HAPs_AmbientConcentrations\\1999\\1999_long.csv",stringsAsFactors = F)
+dat2<-read.csv("S:\\GCMC\\Data\\AirPollution\\HAPs\\AmbientConcentrations\\HAPs_AmbientConcentrations\\1999\\AL_Conc.csv",stringsAsFactors = F)
+dat<-rbind(dat,dat2)
+#dat$GEOID<-paste0(dat$FIPS,dat$TRACT)
+strpdat<-dat[,c("tract_id","hapname","ASPENTotal")]
+widedat<-reshape(strpdat,idvar="tract_id",timevar="hapname",direction='wide')
+names(widedat)[grep(pattern="ASPENTotal.",names(widedat))]<-gsub(pattern="ASPENTotal.","",names(widedat)[grep(pattern="ASPENTotal.",names(widedat))])
+
+widedat2<-merge(x=widedat,y=unique(dat[,c("tract_id","county","uflag")]),by="tract_id",all.x=T)
+write.csv(widedat2,"S:\\GCMC\\Data\\AirPollution\\HAPs\\AmbientConcentrations\\HAPs_AmbientConcentrations\\1999\\1999_Toxics_Ambient_Concentrations_v2.csv")
 
 # ####################################################
 # require(tidycensus)
