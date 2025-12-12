c855b9591683ff032d8dfb0bbf51faac4337b8af
WillhKessler <37121681+WillhKessler@users.noreply.github.com>
2025-12-12 15:17:56 -0500

    Update ParallelSLURM_processingtemplate_v2.R
    update splitting datachunks for csv

--- ParallelSLURM_processingtemplate_v2.R
+++ ParallelSLURM_processingtemplate_v2.R
@@ -75,7 +75,7 @@ batchgrid = function(rasterdir,period,extractionlayer,IDfield,Xfield,Yfield,star
     layername = NA
     weightslayers = NA
     dat<-read.csv(extractionlayer,stringsAsFactors = F)
-    datchunk<-split(dat,f=list(dat[[startdatefield]],dat[[enddatefield]]))
+    datchunk<-split(dat,f=list(dat[[startdatefield]],dat[[enddatefield]]),drop=TRUE)
     datchunk<-lapply(dat,vect,crs="EPSG:4326",geom=c(Xfield,Yfield),keepgeom=T)
     datchunk<-sapply(X = datchunk,FUN = wrap)
   }else if(file_ext(extractionlayer) %in% c("shp","gdb")){
