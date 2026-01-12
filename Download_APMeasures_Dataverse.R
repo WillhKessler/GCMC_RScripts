c9c04da6f8d7f6ad1cdfa73a04357ef52b2e8894
WillhKessler <willhkessler+github@gmail.com>
2026-01-12 10:13:06 -0500

    Updates

--- Download_APMeasures_Dataverse.R
+++ Download_APMeasures_Dataverse.R
@@ -11,54 +11,67 @@ require(terra)
 require(tools)
 require(httr2)
 require(RCurl)
+require(purrr)
 #########################################
-##---Required Inputs---------############
-server = 'dataverse.harvard.edu'
-PM_traccomponents_doi<-"10.7910/DVN/3H7DNP"
+# Set constants
+dataset_doi <- "10.7910/DVN/3H7DNP"
+server <- Sys.getenv("DATAVERSE_SERVER", "dataverse.harvard.edu")
+download_root <- "S:/GCMC/tmp/pmcomp_temp"
 
-destinationdir<-"S:/GCMC/tmp/pmcomp_temp"
-
-# List all files in Repo
-dat<-dataset_files(
-  dataset = PM_traccomponents_doi,
-  server= server,
-)
-
-
-#Existing Files
-success<-basename(list.files(destinationdir,full.name=T,recursive=T))
-
-failed_urls<-c()
-for (file in dat) {
-  if(file[["dataFile"]]$filename %in% success){
-    
-
-    fileID<- file[["dataFile"]]$id
-    
-    fileurl<-get_url_by_id(
-      fileid=fileID,
-      dataset = PM_traccomponents_doi,
-      format = "original",
-      server = server,
-      original = TRUE
-    )
-    
-    out_filename<-file[["dataFile"]]$filename
-    out_dirlabel<-file[["dataFile"]]$directoryLabel
-    if(!is.null(out_dirlabel)){
-    dir.create(path = file.path(destinationdir,out_dirlabel),recursive = T)
-    out_destfile<-file.path(destinationdir,out_dirlabel,out_filename)
-    }
-    else{
-      dir.create(path = file.path(destinationdir))
-      out_destfile<-file.path(destinationdir,out_filename)
-    }
-    tryCatch({
-      download.file(fileurl, 
-                    destfile = out_destfile
-                    )},
-      error=function(e){failed_urls<<-c(failed_urls,fileurl)},
-      warning=function(w){failed_urls<<-c(failed_urls,fileurl)})
-  }  
+# Create base download folder
+if (!dir.exists(download_root)) {
+  dir.create(download_root)
 }
 
+# Get dataset metadata
+ds_info <- get_dataset(dataset = dataset_doi, server = server)
+files_meta <- ds_info$files
+
+# Function to safely construct destination paths
+get_destination_path <- function(fmeta) {
+  
+  # directoryLabel may be NULL or missing
+  dir_label <- fmeta$directoryLabel
+  
+  if (!is.null(dir_label) && nzchar(dir_label)) {
+    # Construct nested directory inside root
+    full_dir <- file.path(download_root, dir_label)
+  } else {
+    # No folder → root
+    full_dir <- download_root
+  }
+  
+  # Create directory if needed
+  if (!dir.exists(full_dir)) {
+    dir.create(full_dir, recursive = TRUE)
+  }
+  
+  # Full path to the destination file
+  file.path(full_dir, fmeta$label)
+}
+
+# Iterate through files and download each
+for(i in 1:nrow(files_meta)){
+  fmeta<-files_meta[i,]
+  
+  dest <- get_destination_path(fmeta)
+  fname <- fmeta$label
+  
+  fid   <- fmeta$id
+  
+  if (!file.exists(dest)) {
+    message("Downloading: ", dest)
+    out<-get_file_by_id(
+      file = fid,
+      dataset = dataset_doi,
+      server = server,
+      destfile = dest,
+      overwrite = FALSE
+    )
+    writeBin(out,dest)
+  } else {
+    message("Exists, skipping: ", dest)
+  }
+}
+
+message("All downloads complete.")
