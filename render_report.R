387e0d4b63a328c4edcfcef629fb3c7a09bd4011
WillhKessler <37121681+WillhKessler@users.noreply.github.com>
2026-01-16 16:53:13 -0500

    Implement argument parsing for report generation
    Add argument handling for report rendering

--- render_report.R
+++ render_report.R
@@ -1,1 +1,16 @@
 
+args <- commandArgs(trailingOnly = TRUE)
+
+if (length(args) == 0) {
+  stop("No arguments supplied", call. = FALSE)
+} else {
+  # Assume the first argument is the value you want to pass
+  argument_value <- args[1]
+}
+
+# Render the R Markdown file, overriding the 'my_arg' parameter
+rmarkdown::render(
+  input = "GenerateReport_cohort_linkage_QAQC.Rmd",
+  params = list(file_name = argument_value)
+)
+
