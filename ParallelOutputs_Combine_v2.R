74588adffb86922e93990b7acb5c03e919d5b34d
WillhKessler <37121681+WillhKessler@users.noreply.github.com>
2026-03-17 10:45:28 -0400

    Update ParallelOutputs_Combine_v2.R

--- ParallelOutputs_Combine_v2.R
+++ ParallelOutputs_Combine_v2.R
@@ -20,7 +20,7 @@ projectname="ExampleLinkage"
     rm(out)
     out3<-Reduce(function(dtf1,dtf2){merge(dtf1,dtf2,all=TRUE)},out2)
   
-    longout<- lapply(out2,function(x){as.data.frame(x%>% pivot_longer(cols= colnames(x)[grepl("^\\d{4}\\-?\\d{2}\\-?\\d{2}\\b",colnames(x))],names_to = "date",values_to = v))})
+    longout<- lapply(out2,function(x){as.data.frame(x%>% pivot_longer(cols= colnames(x)[grepl("^X\\d{4}[.\\-]?\\d{2}[.\\-]?\\d{2}\\b",colnames(x))],names_to = "date",values_to = v))})
     rm(out2)
     longout<-do.call("rbind",longout)
     write.csv(longout,paste(projectname,"_LONG_",v,".csv",sep=""))
